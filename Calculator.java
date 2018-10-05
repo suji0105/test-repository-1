1c1a014ae252975d7f900939566a58d686e36198
최윤주 <yeomju311@gmail.com>
2018-10-05 13:50:27 +0900

    sum 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,12 @@
 // 자바 문법 무시하고 만듭니다..
+// commit, push 해보기
 
 public class Calculator {
     public static void main(String args[]){
-        
+
+    }
+
+    public static int sum(int , int b){
+        return a + b;
     }
 }
